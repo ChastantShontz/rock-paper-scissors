5a52b314aab060795729a56f6ce7faebb5431268
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2021-11-09 07:09:36 -0500

    Set player choice variables
    Created the function choose() and put it inside #rock, #paper, and #scissors.
    
    Gave it a parameter of (rock) for #rock, (paper) for #paper, and (scissors) for #scissors.
    
    Set the var choice to "rock" when (rock), "paper" when (paper), and "scissors" when (scissors).

--- page.js
+++ page.js
@@ -2,6 +2,19 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 const questionMark = document.getElementById("questionMark");
+const resultsTitle = document.getElementById("resultsTitle");
 const winCount = document.getElementById("winCount");
 const loseCount = document.getElementById("loseCount");
 const tieCount = document.getElementById("tieCount");
+
+function choose(rock) {
+  var choice = rock;
+}
+
+function choose(paper) {
+  var choice = paper;
+}
+
+function choose(scissors) {
+  var choice = scissors;
+}
