21cff1e30577024cd7a48dd989691df8120e174a
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2021-11-09 07:44:03 -0500

    Result variable
    Created the function game and named the var result inside of it.
    
    Used an if else statement to set result to win if the player wins, lose if the player looses, and tie if playChoice and compChoice are the same.

--- page.js
+++ page.js
@@ -8,26 +8,50 @@ const loseCount = document.getElementById("loseCount");
 const tieCount = document.getElementById("tieCount");
 
 function playChoose(rock) {
-  var playChoice = rock;
+  var playChoice = "rock";
 }
 
 function playChoose(paper) {
-  var playChoice = paper;
+  var playChoice = "paper";
 }
 
 function playChoose(scissors) {
-  var playChoice = scissors;
+  var playChoice = "scissors";
 }
 
 function compChoose() {
   var number = Math.ceiling(Math.random() * 3);
   if (number == 1) {
-    compChoice = rock;
+    compChoice = "rock";
   }
   else if (number == 2) {
-    compChoice = paper;
+    compChoice = "paper";
   }
   else if (number == 3) {
-    compChoice = scissors;
+    compChoice = "scissors";
+  }
+}
+
+function game() {
+  if ((playChoice == "rock") && (compChoice == "paper")) {
+    var result = "loose";
+  }
+  else if ((playChoice == "rock") && (compChoice == "scissors")) {
+    var result = "win";
+  }
+  else if ((playChoice == "paper") && (compChoice == "rock")) {
+    var result = "win";
+  }
+  else if ((playChoice == "paper") && (compChoice == "scissors")) {
+    var result = "loose";
+  }
+  else if ((playChoice == "scissors") && (compChoice == "rock")) {
+    var result = "loose";
+  }
+  else if ((playChoice == "scissors") && (compChoice == "paper")) {
+    var result = "win";
+  }
+  else if (playChoice == compChoice) {
+    var result = "tie";
   }
 }
