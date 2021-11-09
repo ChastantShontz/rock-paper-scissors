e8ce8c35ac72204bff61d89cbbe70c7edb33204b
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2021-11-09 06:54:40 -0500

    Set constants
    Set the elements that will be changed as JavaScript constants using getElementById.

---
+++ page.js
@@ -0,0 +1,7 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const questionMark = document.getElementById("questionMark");
+const winCount = document.getElementById("winCount");
+const loseCount = document.getElementById("loseCount");
+const tieCount = document.getElementById("tieCount");
